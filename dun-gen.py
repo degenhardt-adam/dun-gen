761d6b4cf60ec82c95f5143f5b1c1d9e98fb7fe4
Adam Degenhardt <degenhardt.adam@gmail.com>
2021-02-23 15:32:04 -0500

    Add branching

--- dun-gen.py
+++ dun-gen.py
@@ -55,20 +55,38 @@ def main():
 
     x = 0
     y = 0
-    direction = Direction.RIGHT
+    direction = Direction.DOWN
+    hall_length = 1
 
+    dungeon[y][x] = Room.START
     while len(cards) > 10:
-        # Add a card to the dungeon
-        dungeon[y][x] = draw()
-
         # Move to the next position
         if direction == Direction.RIGHT:
             x = x + 1
         elif direction == Direction.DOWN:
             y = y + 1
 
-    # Print dungeon
-    print(dungeon)
+        # Add a card to the dungeon
+        dungeon[y][x] = draw()
+
+        # Branch if we need to
+        hall_length = hall_length + 1
+        if hall_length >= 5:
+            if direction == Direction.RIGHT:
+                x = x - randrange(1, 4)
+                direction = Direction.DOWN
+            elif direction == Direction.DOWN:
+                y = y - randrange(1, 4)
+                direction = Direction.RIGHT
+            hall_length = 1
+
+    # Render dungeon
+    for row in dungeon:
+        if not all(room == Room.NONE for room in row):
+            row_render = ''
+            for room in row:
+                row_render = row_render + SPRITES[room] + ' '
+            print(row_render)
 
 
 if __name__ == "__main__":
