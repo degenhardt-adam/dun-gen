75cc807b081075dfc2a0a07510eb5129465ac225
Adam Degenhardt <degenhardt.adam@gmail.com>
2021-02-23 11:26:35 -0500

    Add card deck

--- dun-gen.py
+++ dun-gen.py
@@ -1,6 +1,38 @@
-rooms = [
-    
+from enum import Enum, unique
+from random import randrange
+
+
+@unique
+class Room(Enum):
+    NONE = 0
+    STAIRCASE = 1
+    TREASURE = 2
+    EMPTY = 3
+    SAFE = 4
+    GUARDED_TREASURE = 5
+    HAZARD = 6
+    DEATH = 7
+    ENEMY = 8
+
+
 def main():
+    cards = \
+        [Room.STAIRCASE] * 2 + \
+        [Room.TREASURE] * 4 + \
+        [Room.EMPTY] * 3 + \
+        [Room.SAFE] * 2 + \
+        [Room.GUARDED_TREASURE] + \
+        [Room.HAZARD] * 2 + \
+        [Room.DEATH] + \
+        [Room.ENEMY] * 7
+    
+    def draw():
+        return cards.pop(randrange(len(cards)))
+    
+    while len(cards) > 0:
+        print(draw())
+        
+
 
 if __name__ == "__main__":
     main()
