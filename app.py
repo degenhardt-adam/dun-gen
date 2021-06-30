4ca34bdc7ae6aeb7977796ef49b8941648dbd366
Adam Degenhardt <degenhardt.adam@gmail.com>
2021-06-30 19:49:05 -0400

    Serve kanji image

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from dungen.dungen import generate
-from flask import Flask
+from flask import Flask, send_file
+from pathlib import Path
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return generate()
+
+
+@app.route('/duck')
+def kanji():
+    image = Path('./static/kanji.png')
+    return send_file(image, mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
